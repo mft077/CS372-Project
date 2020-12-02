903762570900fe15ca63e0061baec06335abb1e9
mft077 <43836333+mft077@users.noreply.github.com>
2020-12-02 17:20:47 -0600

    Update RoundData.cs

--- Assets/Scripts/RoundData.cs
+++ Assets/Scripts/RoundData.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Each round have a sries of question
+/*Each round have a sries of question
+Stores the correct answer
+Each round holds a number of question*/
 [System.Serializable]
 public class RoundData
 {
