a2be8e9e4fb567dd48d96f5f92b2fe710608a925
mft077 <43836333+mft077@users.noreply.github.com>
2020-12-02 18:10:02 -0600

    Update MeunScreenController.cs

--- Assets/Scripts/MeunScreenController.cs
+++ Assets/Scripts/MeunScreenController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//When button is pressed Game scene will be loaded
 public class MeunScreenController : MonoBehaviour
 {
     public void StartGame()
